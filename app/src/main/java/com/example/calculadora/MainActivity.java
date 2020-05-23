9eb8a3e86348a531f15575e2f2b20c475e36f6d3
andresperezfc <felipe3apc@gmail.com>
2020-05-23 10:02:31 -0500

    Validación realizada y mensajes bilingues

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -24,57 +24,64 @@ public class MainActivity extends AppCompatActivity {
 
     public void sumar(View v){
         int num_uno, num_dos, sumar;
-        try{
+        if(validar()){
             num_uno = Integer.parseInt(numero_uno.getText().toString());
             num_dos = Integer.parseInt(numero_dos.getText().toString());
             sumar = num_uno + num_dos;
             resultado.setText("" + sumar);
-        }catch (Exception e) {
-            resultado.setText("Ingrese todos los valores");
         }
     }
 
     public void restar(View v){
         int num_uno, num_dos, restar;
-        try{
+        if(validar()){
             num_uno = Integer.parseInt(numero_uno.getText().toString());
             num_dos = Integer.parseInt(numero_dos.getText().toString());
             restar = num_uno - num_dos;
             resultado.setText("" + restar);
-        }catch (Exception e){
-            resultado.setText("Ingrese todos los valores");
         }
     }
 
     public void multiplicar(View v){
         int num_uno, num_dos, multiplicar;
-        try{
+        if(validar()){
             num_uno = Integer.parseInt(numero_uno.getText().toString());
             num_dos = Integer.parseInt(numero_dos.getText().toString());
             multiplicar = num_uno * num_dos;
             resultado.setText("" + multiplicar);
-        }catch (Exception e){
-            resultado.setText("Ingrese todos los valores");
         }
     }
 
     public void dividir(View v){
         int num_uno, num_dos, dividir;
-        try {
+        if(validar()){
             num_uno = Integer.parseInt(numero_uno.getText().toString());
             num_dos = Integer.parseInt(numero_dos.getText().toString());
             dividir = num_uno / num_dos;
             resultado.setText("" + dividir);
-        }catch (Exception e){
-            resultado.setText("Ingrese todos los valores");
         }
-
     }
 
     public void limpiar(View v){
         numero_uno.setText("");
         numero_dos.setText("");
         resultado.setText("");
+    }
+
+    public boolean validar(){
+        String error_numero_uno, error_numero_dos;
+        error_numero_uno = getResources().getString(R.string.error_numero_uno);
+        error_numero_dos = getResources().getString(R.string.error_numero_dos);
+        if(numero_uno.getText().toString().isEmpty()){
+            numero_uno.setError(error_numero_uno);
+            numero_uno.requestFocus();
+            return false;
+        }else if(numero_dos.getText().toString().isEmpty()){
+            numero_dos.setError(error_numero_dos);
+            numero_dos.requestFocus();
+            return false;
+        }
+        return true;
     }
 
 
